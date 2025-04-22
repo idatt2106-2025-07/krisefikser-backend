9500a3070d05f3cf994ac42376931a698416b8d5
stan1sm <st.mockus@gmail.com>
2025-04-22 15:26:53 +0200

    first commit

---
+++ src/main/java/com/group7/krisefikser/KrisefikserApplication.java
@@ -0,0 +1,13 @@
+package com.group7.krisefikser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrisefikserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrisefikserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group7/krisefikser/KrisefikserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group7.krisefikser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrisefikserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
