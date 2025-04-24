804676072a97c99d536f8adb42b45c33883c756b
johanarntsen <johamar@stud.ntnu.no>
2025-04-24 12:56:58 +0200

    refactor: change sendEmail parameter to RequestBody

--- src/main/java/com/group7/krisefikser/controller/EmailController.java
+++ src/main/java/com/group7/krisefikser/controller/EmailController.java
@@ -3,10 +3,7 @@ package com.group7.krisefikser.controller;
 import com.group7.krisefikser.dto.request.EmailRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.group7.krisefikser.service.EmailService;
 
 /**
@@ -21,7 +18,7 @@ public class EmailController {
   private EmailService emailService;
 
   @PostMapping("/send")
-  public ResponseEntity<String> sendEmail(@RequestParam EmailRequest emailRequest) {
+  public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
     try {
       emailService.sendSimpleMessage(
           emailRequest.getTo(),
