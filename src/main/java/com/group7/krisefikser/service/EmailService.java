bd91643a62a3e279a6ef5f06f284fc87b784f46a
johanarntsen <johamar@stud.ntnu.no>
2025-04-25 10:19:43 +0200

    feat: make service-method for sending a template-email

--- src/main/java/com/group7/krisefikser/service/EmailService.java
+++ src/main/java/com/group7/krisefikser/service/EmailService.java
@@ -1,15 +1,20 @@
 package com.group7.krisefikser.service;
 
+import com.group7.krisefikser.enums.EmailTemplateType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
 
   @Autowired
   private JavaMailSender mailSender;
+  @Autowired
+  private EmailTemplateService emailTemplateService;
 
   public void sendSimpleMessage(String to, String subject, String text) {
     SimpleMailMessage message = new SimpleMailMessage();
@@ -18,5 +23,11 @@ public class EmailService {
     message.setSubject(subject);
     message.setText(text);
     mailSender.send(message);
+  }
+
+  public void sendTemplateMessage(String to, EmailTemplateType type, Map<String, String> params) {
+    String subject = emailTemplateService.getSubject(type);
+    String body = emailTemplateService.getBody(type, params);
+    sendSimpleMessage(to, subject, body);
   }
 }
