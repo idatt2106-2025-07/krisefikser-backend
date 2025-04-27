ec9dcc9ac9d90aeb191d790b4832efd443870509
johanarntsen <johamar@stud.ntnu.no>
2025-04-27 15:10:32 +0200

    feat: add response messages for handling login and registering

---
+++ src/main/java/com/group7/krisefikser/enums/AuthResponseMessage.java
@@ -0,0 +1,26 @@
+package com.group7.krisefikser.enums;
+
+public enum AuthResponseMessage {
+  USER_NOT_FOUND("User not found"),
+  INVALID_CREDENTIALS("Invalid credentials"),
+  USER_ALREADY_EXISTS("User already exists"),
+  USER_REGISTERED_SUCCESSFULLY("User registered successfully"),
+  SAVING_USER_ERROR("Error saving user: "),
+  USER_LOGGED_IN_SUCCESSFULLY("User logged in successfully"),
+  USER_LOGIN_ERROR("Error logging in user: "),
+  TOKEN_REFRESH_ERROR("Error refreshing token: "),
+  INVALID_EMAIL_FORMAT("Invalid email format"),
+  PASSWORD_TOO_WEAK("Password is too weak");
+
+
+
+  private final String message;
+
+  AuthResponseMessage(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+}
