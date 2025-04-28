dfcf5744dff0930a951d0147f2564e669eb5806d
Sverre Halvorsen <sverrgha@stud.ntnu.no>
2025-04-28 11:12:55 +0200

    docs: add swagger annotates to the AffectedAreaController and PointOfInterestController

--- src/main/java/com/group7/krisefikser/controller/AffectedAreaController.java
+++ src/main/java/com/group7/krisefikser/controller/AffectedAreaController.java
@@ -2,6 +2,11 @@ package com.group7.krisefikser.controller;
 
 import com.group7.krisefikser.dto.response.AffectedAreaResponse;
 import com.group7.krisefikser.service.AffectedAreaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/affected-area")
 @RequiredArgsConstructor
+@Tag(name = "Affected Area", description = "Endpoints for managing affected areas")
 public class AffectedAreaController {
   private final AffectedAreaService affectedAreaService;
 
@@ -27,6 +33,17 @@ public class AffectedAreaController {
    *
    * @return a list of affected areas
    */
+  @Operation(
+          summary = "Get all affected areas",
+          description = "Retrieves a list of all affected areas.",
+          responses = {
+            @ApiResponse(responseCode = "200", description =
+                          "Successfully retrieved all affected areas",
+                          content = @Content(mediaType = "application/json",
+                                  schema = @Schema(implementation = AffectedAreaResponse.class))),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+          }
+  )
   @GetMapping
   public ResponseEntity<List<AffectedAreaResponse>> getAllAffectedAreas() {
     logger.info("Fetching all affected areas");
