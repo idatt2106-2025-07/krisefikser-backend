5a2cbff32cfb1a4dfd2ae107b5f2dc7097304cc2
johanarntsen <johamar@stud.ntnu.no>
2025-04-28 16:21:02 +0200

    feat: create repository for households

---
+++ src/main/java/com/group7/krisefikser/repository/HouseholdRepository.java
@@ -0,0 +1,33 @@
+package com.group7.krisefikser.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+
+@Repository
+@RequiredArgsConstructor
+public class HouseholdRepository {
+  private final JdbcTemplate jdbcTemplate;
+
+  public Long createHousehold(String name, double longitude, double latitude) {
+    KeyHolder keyHolder = new GeneratedKeyHolder();
+    jdbcTemplate.update(connection -> {
+      PreparedStatement ps = connection.prepareStatement(
+          "INSERT INTO households (name, longitude, latitude) VALUES (?, ?, ?)",
+          Statement.RETURN_GENERATED_KEYS
+      );
+      ps.setString(1, name);
+      ps.setDouble(2, longitude);
+      ps.setDouble(3, latitude);
+      return ps;
+    }, keyHolder);
+
+    return keyHolder.getKey().longValue();
+  }
+}
