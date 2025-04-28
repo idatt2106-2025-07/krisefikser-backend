7da6f52bc2c21f57514b6bec684dd6c2099c642c
johanarntsen <johamar@stud.ntnu.no>
2025-04-28 09:58:14 +0200

    feat: make methods in User repo

--- src/main/java/com/group7/krisefikser/dto/request/RegisterRequest.java
+++ src/main/java/com/group7/krisefikser/dto/request/RegisterRequest.java
@@ -1,5 +1,6 @@
 package com.group7.krisefikser.dto.request;
 
+import com.group7.krisefikser.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,4 +12,5 @@ public class RegisterRequest {
   private String phoneNumber;
   private String password;
   private Long householdId;
+  private Role role;
 }
--- src/main/java/com/group7/krisefikser/repository/UserRepository.java
+++ src/main/java/com/group7/krisefikser/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.group7.krisefikser.repository;
 
+import com.group7.krisefikser.enums.Role;
 import com.group7.krisefikser.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -20,13 +21,23 @@ public class UserRepository {
 
 
   public Optional<User> findByEmail(String email) {
-    // Implementation to find user by email
-    return null;
+    String sql = "SELECT * FROM user WHERE email = ?";
+    return jdbcTemplate.query(sql, (rs, rowNum) -> {
+      User user = new User();
+      user.setId(rs.getLong("id"));
+      user.setEmail(rs.getString("email"));
+      user.setName(rs.getString("name"));
+      user.setHouseholdId(rs.getLong("household_id"));
+      user.setPassword(rs.getString("password"));
+      user.setRole(Role.valueOf(rs.getString("role")));
+      return user;
+    }, email).stream().findFirst();
   }
 
   public Optional<User> save(User user) {
-    // Implementation to save user
-    return null;
+    String query = "INSERT INTO users (email, name, household_id, password, role) VALUES (?, ?, ?, ?, ?)";
+    jdbcTemplate.update(query, user.getEmail(), user.getName(), user.getHouseholdId(), user.getPassword(), user.getRole());
+    return findByEmail(user.getEmail());
   }
 
   public Optional<Object> findById(Long id) {
