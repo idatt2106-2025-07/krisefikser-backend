4945382c932d32dea1d304e150d5cd95f2329691
emilruud <emilruud@ntnu.no>
2025-04-30 14:31:06 +0200

    feat: add documentation for JoinHouseholdRequest class

--- src/main/java/com/group7/krisefikser/model/JoinHouseholdRequest.java
+++ src/main/java/com/group7/krisefikser/model/JoinHouseholdRequest.java
@@ -4,6 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a user entity in the system.
+ * This class contains user details such as email, name, household association, and role.
+ * Annotations:
+ * - @Data: Generates getters, setters, toString, equals, and hashCode methods.
+ * - @AllArgsConstructor: Generates a constructor with all fields as parameters.
+ * - @NoArgsConstructor: Generates a no-argument constructor.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
