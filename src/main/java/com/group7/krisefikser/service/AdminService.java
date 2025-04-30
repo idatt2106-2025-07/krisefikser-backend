c8f43fd0e8c05502f682beeef8b3074568c2051b
Vetle <vetletb@stud.ntnu.no>
2025-04-30 09:42:45 +0200

    docs: add missing javadoc to AdminService

--- src/main/java/com/group7/krisefikser/service/AdminService.java
+++ src/main/java/com/group7/krisefikser/service/AdminService.java
@@ -1,6 +1,7 @@
 package com.group7.krisefikser.service;
 
 import com.group7.krisefikser.dto.request.InviteAdminRequest;
+import com.group7.krisefikser.dto.request.RegisterAdminRequest;
 import com.group7.krisefikser.enums.EmailTemplateType;
 import com.group7.krisefikser.exception.JwtMissingPropertyException;
 import com.group7.krisefikser.utils.JwtUtils;
@@ -10,6 +11,11 @@ import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling admin-related operations.
+ * The role of this service is to manage the business logic related to
+ * admin operations, such as inviting and registering admins.
+ */
 @Service
 @RequiredArgsConstructor
 public class AdminService {
@@ -18,6 +24,12 @@ public class AdminService {
 
   private final JwtUtils jwtUtils;
 
+  /**
+   * Invites an admin by generating a jwt invite token and sending an email with the invite link.
+   *
+   * @param request The request containing the email of the admin to be invited.
+   * @throws JwtMissingPropertyException if there is an issue with the JWT properties.
+   */
   public void inviteAdmin(InviteAdminRequest request) throws JwtMissingPropertyException {
     String username = "admin" + generateShortenedUuid();
 
@@ -32,6 +44,21 @@ public class AdminService {
     );
   }
 
+  /**
+   * Registers an admin by validating the invite token and creating a new admin account.
+   *
+   * @param request The request containing the invite token and other registration details.
+   * @throws JwtMissingPropertyException if there is an issue with the JWT properties.
+   */
+  public void registerAdmin(RegisterAdminRequest request) throws JwtMissingPropertyException {
+    String username = jwtUtils.validateInviteTokenAndGetUsername(request.getToken());
+  }
+
+  /**
+   * Generates a shortened UUID for the admin username.
+   *
+   * @return A shortened UUID as a string.
+   */
   private String generateShortenedUuid() {
     UUID uuid = UUID.randomUUID();
 
