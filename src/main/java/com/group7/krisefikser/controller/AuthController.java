1ae5da2dd802445b659e7e1adf2c1f864f9cd9da
johanarntsen <johamar@stud.ntnu.no>
2025-05-01 17:26:02 +0200

    feat: make endpoint for verifying a user

--- src/main/java/com/group7/krisefikser/controller/AuthController.java
+++ src/main/java/com/group7/krisefikser/controller/AuthController.java
@@ -10,9 +10,11 @@ import java.util.logging.Logger;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -38,7 +40,7 @@ public class AuthController {
    */
   @PostMapping("/register")
   public ResponseEntity<AuthResponse> registerUser(
-      @RequestBody RegisterRequest request, HttpServletResponse response) {
+            @RequestBody RegisterRequest request, HttpServletResponse response) {
     logger.info("Received register request for user: " + request.getEmail());
     try {
       AuthResponse authResponse = userService.registerUser(request, response);
@@ -77,6 +79,36 @@ public class AuthController {
       logger.warning("Error logging in user: " + e.getMessage());
       return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
           new AuthResponse(AuthResponseMessage.USER_LOGIN_ERROR.getMessage()
+                  + e.getMessage(), null, null));
+    }
+  }
+
+  /**
+   * Endpoint for email verification.
+   * This method handles the verification of a user's email.
+   * It accepts a token as a request parameter.
+   *
+   * @param token the verification token
+   * @return a ResponseEntity containing the authentication response
+   */
+  @GetMapping("/verify-email")
+  public ResponseEntity<AuthResponse> verifyEmail(@RequestParam String token) {
+    logger.info("Received email verification request with token: " + token);
+    try {
+      AuthResponse authResponse = userService.verifyEmail(token);
+
+      if (authResponse.getMessage().equals(
+          AuthResponseMessage.USER_VERIFIED_SUCCESSFULLY.getMessage())) {
+        logger.info("User verified successfully with token: " + token);
+        return ResponseEntity.ok(authResponse);
+      } else {
+        logger.warning("Error verifying user: " + authResponse.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(authResponse);
+      }
+    } catch (Exception e) {
+      logger.warning("Error verifying email: " + e.getMessage());
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(
+          new AuthResponse(AuthResponseMessage.EMAIL_VERIFICATION_ERROR.getMessage()
                   + e.getMessage(), null, null));
     }
   }
