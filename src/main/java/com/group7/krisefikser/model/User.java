38f3ea4737ed152aef72027c6a61d4074ebb95dd
johanarntsen <johamar@stud.ntnu.no>
2025-05-01 11:01:56 +0200

    refactor: remove unacessary contructors

--- src/main/java/com/group7/krisefikser/model/User.java
+++ src/main/java/com/group7/krisefikser/model/User.java
@@ -21,51 +21,5 @@ public class User {
   private Long householdId;
   private String password;
   private Role role;
-
-  /**
-   * Constructor for User class. This constructor is used to create a new user object.
-   *
-   * @param email    the email of the user
-   * @param name     the name of the user
-   * @param password the password of the user
-   * @param role     the role of the user
-   */
-  public User(String email, String name, String password, Long householdId, Role role) {
-    this.email = email;
-    this.name = name;
-    this.password = password;
-    this.householdId = householdId;
-    this.role = role;
-  }
-
-  /**
-   * Constructor for User class. This constructor is used to create a new user object
-   * without a role.
-   *
-   * @param email   the email of the user
-   * @param name    the name of the user
-   * @param hashedPassword the hashed password of the user
-   * @param householdId the household ID of the user
-   */
-  public User(String email, String name, String hashedPassword, Long householdId) {
-    this.name = name;
-    this.email = email;
-    this.password = hashedPassword;
-    this.householdId = householdId;
-  }
-
-  /**
-   * Constructor for User class. This constructor is used to create a new user object
-   * without a household ID and role.
-   *
-   * @param email  the email of the user
-   * @param name   the name of the user
-   * @param hashedPassword the hashed password of the user
-   */
-  public User(String email, String name, String hashedPassword) {
-    this.name = name;
-    this.email = email;
-    this.password = hashedPassword;
-  }
 }
 
