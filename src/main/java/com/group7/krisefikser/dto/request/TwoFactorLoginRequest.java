aeaf13e9148f351884ef23b32ee19bc8ffeb298f
Vetle <vetletb@stud.ntnu.no>
2025-05-02 13:47:04 +0200

    feat: add request validation

--- src/main/java/com/group7/krisefikser/dto/request/TwoFactorLoginRequest.java
+++ src/main/java/com/group7/krisefikser/dto/request/TwoFactorLoginRequest.java
@@ -1,8 +1,10 @@
 package com.group7.krisefikser.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class TwoFactorLoginRequest {
+  @NotBlank(message = "Token is required")
   private String token;
 }
