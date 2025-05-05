604fc6522c51d1c56cdf7ca7ec524c80cc3a3ca5
Vetle <vetletb@stud.ntnu.no>
2025-05-05 15:20:48 +0200

    feat: add mapping between new dto and UserPosition model

--- src/main/java/com/group7/krisefikser/mapper/UserPositionMapper.java
+++ src/main/java/com/group7/krisefikser/mapper/UserPositionMapper.java
@@ -1,6 +1,7 @@
 package com.group7.krisefikser.mapper;
 
 import com.group7.krisefikser.dto.request.SharePositionRequest;
+import com.group7.krisefikser.dto.response.HouseholdMemberPositionResponse;
 import com.group7.krisefikser.model.UserPosition;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -13,4 +14,13 @@ public interface UserPositionMapper {
   @Mapping(source = "latitude", target = "latitude")
   @Mapping(source = "longitude", target = "longitude")
   UserPosition sharePositionRequestToUserPosition(SharePositionRequest request);
+
+  @Mapping(source = "latitude", target = "latitude")
+  @Mapping(source = "longitude", target = "longitude")
+  @Mapping(source = "name", target = "name")
+  HouseholdMemberPositionResponse userPositionToHouseholdMemberPositionResponse(
+      UserPosition userPosition);
+
+  HouseholdMemberPositionResponse[] userPositionArrayToHouseholdMemberPositionResponseArray(
+      UserPosition[] userPositions);
 }
