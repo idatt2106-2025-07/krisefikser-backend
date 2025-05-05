acc924a20701d755bf471f79223eabf84967ffbc
Vetle <vetletb@stud.ntnu.no>
2025-05-05 15:19:10 +0200

    feat: create HouseholdMemberPositionResponse dto

---
+++ src/main/java/com/group7/krisefikser/dto/response/HouseholdMemberPositionResponse.java
@@ -0,0 +1,10 @@
+package com.group7.krisefikser.dto.response;
+
+import lombok.Data;
+
+@Data
+public class HouseholdMemberPositionResponse {
+  private String latitude;
+  private String longitude;
+  private String name;
+}
