4065691f1263de3367eac7dcaa7400df9affe651
Vetle <vetletb@stud.ntnu.no>
2025-05-05 13:39:40 +0200

    feat: add user position table to database

---
+++ src/main/resources/db/migration/V5__create_user_position_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE user_position (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT            NOT NULL,
+    longitude DOUBLE       NOT NULL,
+    latitude  DOUBLE       NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    INDEX (user_id)
+);
+
+ALTER TABLE users
+    ADD COLUMN share_position BOOLEAN NOT NULL DEFAULT FALSE;
