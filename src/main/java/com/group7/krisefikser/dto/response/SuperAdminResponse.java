f70608e76914c503a5b7a43bff2aafbf50349191
johanarntsen <johamar@stud.ntnu.no>
2025-05-05 14:37:40 +0200

    feat: make response for superadmin

---
+++ src/main/java/com/group7/krisefikser/dto/response/SuperAdminResponse.java
@@ -0,0 +1,11 @@
+package com.group7.krisefikser.dto.response;
+
+import com.group7.krisefikser.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SuperAdminResponse {
+  private String email;
+}
