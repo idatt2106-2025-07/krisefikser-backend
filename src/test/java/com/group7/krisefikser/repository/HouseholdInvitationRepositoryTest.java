e9b8793441d382797c2638d09be0c64f665571a6
emilruud <emilruud@ntnu.no>
2025-05-05 15:29:12 +0200

    refactor: remove unused import

--- src/test/java/com/group7/krisefikser/repository/HouseholdInvitationRepositoryTest.java
+++ src/test/java/com/group7/krisefikser/repository/HouseholdInvitationRepositoryTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 
