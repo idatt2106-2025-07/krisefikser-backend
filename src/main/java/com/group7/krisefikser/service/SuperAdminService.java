e27bff3d54ee15ed9798cc7bb896ad388dc6f98a
johanarntsen <johamar@stud.ntnu.no>
2025-05-05 13:17:50 +0200

    feat: make methods for getting all admins and deleting admin

--- src/main/java/com/group7/krisefikser/service/SuperAdminService.java
+++ src/main/java/com/group7/krisefikser/service/SuperAdminService.java
@@ -1,21 +1,15 @@
 package com.group7.krisefikser.service;
 
 import com.group7.krisefikser.dto.response.AuthResponse;
-import com.group7.krisefikser.dto.response.SuperAdminResponse;
 import com.group7.krisefikser.enums.AuthResponseMessage;
-import com.group7.krisefikser.enums.EmailTemplateType;
 import com.group7.krisefikser.enums.Role;
 import com.group7.krisefikser.model.User;
-import com.group7.krisefikser.repository.ItemRepo;
 import com.group7.krisefikser.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -23,6 +17,34 @@ public class SuperAdminService {
 
   private UserRepository userRepository;
   private UserService userService;
+
+  public List<User> getAllAdmins() {
+    try {
+      List<User> admins = userRepository.findByRole(Role.ROLE_ADMIN);
+      if (admins.isEmpty()) {
+        throw new IllegalArgumentException("No admins found");
+      }
+      return admins;
+    } catch (Exception e) {
+      throw new RuntimeException("Error fetching admins: " + e.getMessage());
+    }
+  }
+
+  public void deleteAdmin(Long adminId) {
+    try {
+      Optional<User> user = userRepository.findById(adminId);
+      if (user.isPresent()) {
+        if (!user.get().getRole().equals(Role.ROLE_ADMIN)) {
+          throw new IllegalArgumentException("User is not an admin");
+        }
+        userRepository.deleteById(user.get().getId());
+      } else {
+        throw new IllegalArgumentException("User not found");
+      }
+    } catch (Exception e) {
+      throw new RuntimeException("Error deleting admin: " + e.getMessage());
+    }
+  }
 
   public AuthResponse sendResetPasswordEmailToAmdmin(String email) {
     try {
