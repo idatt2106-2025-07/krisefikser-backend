a0a4c7e0b5ebe6b7e39199abaa48458d6a7f3eb5
Vetle <vetletb@stud.ntnu.no>
2025-05-05 15:21:52 +0200

    feat: add get household member positions in service

--- src/main/java/com/group7/krisefikser/service/UserPositionService.java
+++ src/main/java/com/group7/krisefikser/service/UserPositionService.java
@@ -1,6 +1,7 @@
 package com.group7.krisefikser.service;
 
 import com.group7.krisefikser.dto.request.SharePositionRequest;
+import com.group7.krisefikser.dto.response.HouseholdMemberPositionResponse;
 import com.group7.krisefikser.mapper.UserPositionMapper;
 import com.group7.krisefikser.model.UserPosition;
 import com.group7.krisefikser.repository.UserPositionRepository;
@@ -13,12 +14,32 @@ import org.springframework.stereotype.Service;
 public class UserPositionService {
 
   private final UserPositionRepository positionRepository;
+  private final UserPositionRepository userPositionRepository;
 
   public void sharePosition(SharePositionRequest request) {
     UserPosition userPosition =
         UserPositionMapper.INSTANCE.sharePositionRequestToUserPosition(request);
     String userId = SecurityContextHolder.getContext().getAuthentication().getName();
     userPosition.setUserId(Long.parseLong(userId));
-    positionRepository.updateUserPosition(userPosition);
+
+    boolean isSharingPosition = positionRepository.isSharingPosition(userPosition.getUserId());
+
+    if (!isSharingPosition) {
+      userPositionRepository.addUserPosition(userPosition);
+    } else {
+      userPositionRepository.updateUserPosition(userPosition);
+    }
+  }
+
+  public boolean isSharingPosition() {
+    String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+    return positionRepository.isSharingPosition(Long.parseLong(userId));
+  }
+
+  public HouseholdMemberPositionResponse[] getHouseholdPositions() {
+    String userId = SecurityContextHolder.getContext().getAuthentication().getName();
+    UserPosition[] userPositions = positionRepository.getHouseholdPositions(Long.parseLong(userId));
+    return UserPositionMapper.INSTANCE.userPositionArrayToHouseholdMemberPositionResponseArray(
+            userPositions);
   }
 }
