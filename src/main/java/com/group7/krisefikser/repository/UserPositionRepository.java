6eda3bad99ad46f55c9ddea8d73ebd2438b66cc3
Vetle <vetletb@stud.ntnu.no>
2025-05-05 13:58:43 +0200

    feat: add is sharing, add and delete position to repo

--- src/main/java/com/group7/krisefikser/repository/UserPositionRepository.java
+++ src/main/java/com/group7/krisefikser/repository/UserPositionRepository.java
@@ -16,4 +16,29 @@ public class UserPositionRepository {
     jdbcTemplate.update(
         sql, userPosition.getLatitude(), userPosition.getLongitude(), userPosition.getUserId());
   }
+
+  public void addUserPosition(UserPosition userPosition) {
+    String sql = "INSERT INTO user_position (latitude, longitude, user_id) VALUES (?, ?, ?)";
+    jdbcTemplate.update(
+        sql, userPosition.getLatitude(), userPosition.getLongitude(), userPosition.getUserId());
+  }
+
+  public void deleteUserPosition(Long userId) {
+    String sql = "DELETE FROM user_position WHERE user_id = ?";
+    jdbcTemplate.update(sql, userId);
+  }
+
+  public boolean isSharingPosition(Long userId) {
+    String sql =
+        "SELECT CASE "
+            + "WHEN EXISTS ( "
+            + "SELECT 1 FROM user_position WHERE user_id = ? "
+            + ") THEN 'true' "
+            + "ELSE 'false' "
+            + "END AS finnes";
+    return jdbcTemplate.queryForObject(
+        sql,
+        new Object[] {userId},
+        Boolean.class);
+  }
 }
