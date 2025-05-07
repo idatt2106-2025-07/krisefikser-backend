878ca952d4e55f181e358443a63032639feda694
Vetle <vetletb@stud.ntnu.no>
2025-05-07 11:20:00 +0200

    refactor: delete unused dto

--- src/main/java/com/group7/krisefikser/dto/request/GetRegisteredPrivacyPolicyRequest.java
+++
@@ -1,8 +0,0 @@
-package com.group7.krisefikser.dto.request;
-
-import lombok.Data;
-
-@Data
-public class GetRegisteredPrivacyPolicyRequest {
-  private String registered;
-}
--- src/main/java/com/group7/krisefikser/dto/request/GetUnregisteredPrivacyPolicyRequest.java
+++
@@ -1,8 +0,0 @@
-package com.group7.krisefikser.dto.request;
-
-import lombok.Data;
-
-@Data
-public class GetUnregisteredPrivacyPolicyRequest {
-  private String unregistered;
-}
