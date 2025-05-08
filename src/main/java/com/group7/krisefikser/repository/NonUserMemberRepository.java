80c89b886c553de985fdbf250df3e8e5a35ed097
Vetle <vetletb@stud.ntnu.no>
2025-05-08 09:08:31 +0200

    feat: add get non-user members by household in NonUserMemberRepository

--- src/main/java/com/group7/krisefikser/repository/NonUserMemberRepository.java
+++ src/main/java/com/group7/krisefikser/repository/NonUserMemberRepository.java
@@ -34,4 +34,26 @@ public class NonUserMemberRepository {
     String sql = "DELETE FROM non_user_members WHERE id = ? AND household_id = ?";
     jdbcTemplate.update(sql, id, householdId);
   }
+
+  /**
+   * Retrieves a list of non-user members associated with a specific household.
+   *
+   * @param householdId The ID of the household for which to retrieve non-user members.
+   * @return A list of NonUserMember objects associated with the specified household.
+   */
+  public List<NonUserMember> getNonUserMembersByHousehold(long householdId) {
+    String sql = "SELECT * FROM non_user_members WHERE household_id = ?";
+
+    return jdbcTemplate.query(
+        sql,
+        new Object[] {householdId},
+        (rs, rowNum) -> {
+          NonUserMember member = new NonUserMember();
+          member.setId(rs.getLong("id"));
+          member.setName(rs.getString("name"));
+          member.setHouseholdId(rs.getLong("household_id"));
+          member.setType(NonUserMemberType.valueOf(rs.getString("type").toUpperCase()));
+          return member;
+        });
+  }
 }
